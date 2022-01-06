b2e4abd3f3e122cbf040de9813adf43248a4e621
Adam Bailey <adamjbailey7@gmail.com>
2022-01-06 07:10:28 -0800

    improve accessibility of scroll to top button

--- resources/js/Jetstream/ScrollTop.vue
+++ resources/js/Jetstream/ScrollTop.vue
@@ -4,15 +4,15 @@
         class="invisible sticky w-full flex justify-end bottom-0 pb-3 pr-5 lg:pr-16 transition"
     >
         <div
-            class="bg-white bg-opacity-60 rounded-full cursor-pointer text-gray-400 hover:text-blue-400 transition"
+            class="rounded-full cursor-pointer text-gray-400 hover:text-blue-400 transition"
         >
-            <a
+            <button
                 role="button"
-                aria-labelledby="scroll to top of the page"
+                aria-label="scroll to top of the page"
                 @click="scrollToTop"
             >
                 <i class="text-4xl md:text-5xl ri-arrow-up-circle-line"></i>
-            </a>
+            </button>
         </div>
     </div>
 </template>
