3081900ef0afe259f0796f7eb2fa63a3fae0ddc2
Adam Bailey <adamjbailey7@gmail.com>
2022-01-09 12:45:17 -0800

    create basic dashboard view for super admins

---
+++ resources/js/Layouts/DashboardLayout.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <Head :title="title" />
+
+        <banner />
+
+        <div class="bg-gray-100">
+            <primary-nav />
+
+            <div class="min-h-screen grid grid-cols-1 md:grid-cols-[15rem,1fr]">
+                <div class="aside bg-gray-800 text-gray-100">
+                    <dashboard-nav></dashboard-nav>
+                </div>
+                <div class="main">
+                    <slot></slot>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { Head } from "@inertiajs/inertia-vue3";
+import PrimaryNav from "@/Layouts/Nav/PrimaryNav";
+import DashboardNav from "@/Layouts/Nav/DashboardNav";
+import Banner from "@/Jetstream/Banner";
+
+export default defineComponent({
+    components: {
+        Banner,
+        PrimaryNav,
+        DashboardNav,
+        Head,
+    },
+    props: {
+        title: String,
+    },
+});
+</script>
---
+++ resources/js/Layouts/Nav/DashboardNav.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1 class="text-2xl font-semibold p-3 border-b">Dashboard</h1>
+    <ul>
+        <li class="p-5 border-b">
+            <Link class="w-full"> Active Chapters </Link>
+        </li>
+    </ul>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { Link } from "@inertiajs/inertia-vue3";
+
+export default defineComponent({
+    components: { Link },
+});
+</script>
