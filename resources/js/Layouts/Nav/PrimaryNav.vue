a95e9722387bf2969cf7b6e72ca5765e0ef0e5f3
Adam Bailey <adamjbailey7@gmail.com>
2022-01-13 10:23:03 -0800

    only show switch teams if there is more than 1 team

--- resources/js/Layouts/Nav/PrimaryNav.vue
+++ resources/js/Layouts/Nav/PrimaryNav.vue
@@ -104,50 +104,60 @@
 
                                         <!-- Team Switcher -->
                                         <div
-                                            class="block px-4 py-2 text-xs text-gray-400"
+                                            v-if="
+                                                $page.props.user.all_teams
+                                                    .length > 1
+                                            "
                                         >
-                                            Switch Teams
-                                        </div>
-
-                                        <template
-                                            v-for="team in $page.props.user
-                                                .all_teams"
-                                            :key="team.id"
-                                        >
-                                            <form
-                                                @submit.prevent="
-                                                    switchToTeam(team)
-                                                "
+                                            <div
+                                                class="block px-4 py-2 text-xs text-gray-400"
                                             >
-                                                <jet-dropdown-link as="button">
-                                                    <div
-                                                        class="flex items-center"
+                                                Switch Teams
+                                            </div>
+
+                                            <template
+                                                v-for="team in $page.props.user
+                                                    .all_teams"
+                                                :key="team.id"
+                                            >
+                                                <form
+                                                    @submit.prevent="
+                                                        switchToTeam(team)
+                                                    "
+                                                >
+                                                    <jet-dropdown-link
+                                                        as="button"
                                                     >
-                                                        <svg
-                                                            v-if="
-                                                                team.id ===
-                                                                $page.props.user
-                                                                    .current_team_id
-                                                            "
-                                                            class="mr-2 h-5 w-5 text-green-400"
-                                                            fill="none"
-                                                            stroke-linecap="round"
-                                                            stroke-linejoin="round"
-                                                            stroke-width="2"
-                                                            stroke="currentColor"
-                                                            viewBox="0 0 24 24"
+                                                        <div
+                                                            class="flex items-center"
                                                         >
-                                                            <path
-                                                                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                                                            ></path>
-                                                        </svg>
-                                                        <div>
-                                                            {{ team.name }}
+                                                            <svg
+                                                                v-if="
+                                                                    team.id ===
+                                                                    $page.props
+                                                                        .user
+                                                                        .current_team_id
+                                                                "
+                                                                class="mr-2 h-5 w-5 text-green-400"
+                                                                fill="none"
+                                                                stroke-linecap="round"
+                                                                stroke-linejoin="round"
+                                                                stroke-width="2"
+                                                                stroke="currentColor"
+                                                                viewBox="0 0 24 24"
+                                                            >
+                                                                <path
+                                                                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                                                                ></path>
+                                                            </svg>
+                                                            <div>
+                                                                {{ team.name }}
+                                                            </div>
                                                         </div>
-                                                    </div>
-                                                </jet-dropdown-link>
-                                            </form>
-                                        </template>
+                                                    </jet-dropdown-link>
+                                                </form>
+                                            </template>
+                                        </div>
                                     </template>
                                 </div>
                             </template>
