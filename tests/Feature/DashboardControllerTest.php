658683b9497580be66c1ac4546287fd90f879659
Adam Bailey <adamjbailey7@gmail.com>
2022-02-15 10:05:50 -0800

    Upgrade all dependencies and apply all required changes for laravel 9

--- tests/Feature/DashboardControllerTest.php
+++ tests/Feature/DashboardControllerTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Inertia\Testing\Assert;
+use Inertia\Testing\AssertableInertia as Assert;
 use Tests\TestCase;
 
 class DashboardControllerTest extends TestCase
